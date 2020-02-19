d74b4b5ff08e6f593467b721da0c07e5ffb05326
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-02-19 18:47:38 -0300

    added code for CRUD operations but error preventing server from running

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const carsRouter = require('./cars/carRouter');
+
+const db = require('./data/car-dealer.db3');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/cars', carsRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h3>DB Helpers with knex</h3>');
+})
+
+module.exports = server;
